ad55c5c62dee6a9cb326bd829fd2a1802b1a76bc
kunbu0203 <59350831+kunbu0203@users.noreply.github.com>
2023-09-19 09:19:53 +0800

    tigerair 9th 0919

---
+++ tigerair-9th/assets/js/all.js
@@ -0,0 +1,48 @@
+// Avoid `console` errors in browsers that lack a console.
+(function () {
+  var method;
+
+  var noop = function noop() {};
+
+  var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'];
+  var length = methods.length;
+  var console = window.console = window.console || {};
+
+  while (length--) {
+    method = methods[length]; // Only stub undefined methods.
+
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+})(); // Place any jQuery/helper plugins in here.
+
+
+(function (targetWidth) {
+  var deviceWidth = screen.width;
+  var ratio = deviceWidth / targetWidth;
+  var viewport = document.querySelector('meta[name="viewport"]');
+
+  if (ratio < 1) {
+    viewport.setAttribute('content', 'width=device-width, initial-scale=' + ratio + ', minimum-scale=' + ratio + ', maximum-scale=' + ratio + ', user-scalable=yes');
+  }
+})(375);
+
+$('.header-burger').on('click', function (e) {
+  e.preventDefault();
+  $('body').toggleClass('-menuOpen');
+});
+new Swiper('.step-slider', {
+  slidesPerView: 'auto'
+});
+$('[data-anchor-btn]').on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $('[data-anchor-block="' + $(this).data('anchor-btn') + '"]').offset().top
+  }, 300);
+  $('body').removeClass('-menuOpen');
+});
+AOS.init({
+  duration: 800,
+  once: true
+});
